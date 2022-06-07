4d27c5adf2fe7a0758a743f601d322bd8866f10c
Sonam.Singhal <sonam.singhal@unthinkable.co>
2022-06-07 21:08:18 +0530

    Add project files.

---
+++ BlazorExample/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorExample
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public string dateTime { get; set; } = DateTime.Today.ToShortDateString();
+
+    }
+}
